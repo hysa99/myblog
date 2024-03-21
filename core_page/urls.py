d4d0c6b8eb9b9ea9892e1896bebd5e7fab8b8f28
hysa99 <eneahysa4@gmail.com>
2024-03-21 21:44:03 +0100

    new changes

--- core_page/urls.py
+++ core_page/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("<int:pk>/", views.detail, name="detail"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
